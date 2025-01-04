58859e8c15ab4a7e364cb9e3d19a931c8e041758
kitelev <kitelev.andrey@gmail.com>
2025-01-04 23:38:14 +0500

    Add extra tables to DailyNoteLayout

--- app/src/adapters/input/layouts/DailyNoteLayout.ts
+++ app/src/adapters/input/layouts/DailyNoteLayout.ts
@@ -5,75 +5,77 @@ import {EffortStatusComparator} from "../../../../../core/src/domain/effort/Effo
 import Comparator from "../../../../../common/Comparator";
 import AbstractLayout from "./AbstractLayout";
 import DailyNote from "../../../../../core/src/domain/DailyNote";
+import {Predicate} from "../../../../../common/Predicate";
 
 export default class DailyNoteLayout extends AbstractLayout<DailyNote> {
+
 	constructor(ctx: ExoContext, dvRenderer: DvRenderer) {
 		super(ctx, dvRenderer);
 	}
 
 	async render(ko: DailyNote, el: HTMLElement): Promise<void> {
-		await this.plannedStartToday(el);
-		await this.plannedStartBefore(el);
-		await this.due(el);
+		const filters: Map<string, (e: Effort) => boolean> = new Map();
+		const day = ko.date;
 
-		// Efforts with started = today
-		// Efforts with ended = today
-	}
-
-	private async plannedStartToday(el: HTMLElement) {
-		const today = new Date();
-		const efforts = await this.ctx.effortRepository.find(e => {
+		filters.set("plannedStartToday", e => {
 			if (e.plannedStart === null) {
 				return false;
 			}
-			return e.plannedStart.setHours(0, 0, 0, 0) === today.setHours(0, 0, 0, 0);
+			return e.plannedStart.setHours(0, 0, 0, 0) === day.setHours(0, 0, 0, 0);
 		});
-		if (efforts.length > 0) {
-			let header = this.createH1("Planned start today");
-			el.appendChild(header);
 
-			const dvTable = await this.createTable(efforts);
-			el.appendChild(dvTable)
-		}
-	}
-
-	private async plannedStartBefore(el: HTMLElement) {
-		const today = new Date();
-		const efforts = await this.ctx.effortRepository.find(e => {
+		filters.set("plannedStartBefore", e => {
 			if (e.plannedStart === null || e.isResolved()) {
 				return false;
 			}
-			return e.plannedStart.setHours(0, 0, 0, 0) < today.setHours(0, 0, 0, 0);
+			return e.plannedStart.setHours(0, 0, 0, 0) < day.setHours(0, 0, 0, 0);
 		});
-		if (efforts.length > 0) {
-			let header = this.createH1("Planned start before");
-			el.appendChild(header);
 
-			const dvTable = await this.createTable(efforts);
-			el.appendChild(dvTable)
-		}
-	}
-
-	private async due(el: HTMLElement) {
-		const today = new Date();
-		const todayEfforts = await this.ctx.effortRepository.find(e => {
+		filters.set("due", e => {
 			if (e.due === null || e.isResolved()) {
 				return false;
 			}
-			return e.due.setHours(0, 0, 0, 0) <= today.setHours(0, 0, 0, 0);
+			return e.due.setHours(0, 0, 0, 0) <= day.setHours(0, 0, 0, 0);
 		});
-		if (todayEfforts.length > 0) {
-			let header = this.createH1("Due today or before");
+
+		filters.set("started", e => {
+			if (e.started === null || e.isResolved()) {
+				return false;
+			}
+			return e.started.setHours(0, 0, 0, 0) === day.setHours(0, 0, 0, 0);
+		});
+
+		filters.set("ended", e => {
+			if (e.ended === null || e.isResolved()) {
+				return false;
+			}
+			return e.ended.setHours(0, 0, 0, 0) === day.setHours(0, 0, 0, 0);
+		});
+
+		const efforts = await this.ctx.effortRepository.find(e => {
+			return Array.from(filters.values()).some(f => f(e));
+		});
+
+		await this.printEfforts(efforts, el, filters.get("plannedStartToday")!, "Planned start today");
+		await this.printEfforts(efforts, el, filters.get("plannedStartBefore")!, "Planned start before");
+		await this.printEfforts(efforts, el, filters.get("due")!, "Due today or before");
+		await this.printEfforts(efforts, el, filters.get("started")!, "Started today");
+		await this.printEfforts(efforts, el, filters.get("ended")!, "Ended today");
+	}
+
+	private async printEfforts(allEfforts: Effort[], el: HTMLElement, predicate: Predicate<Effort>, title: string) {
+		const efforts = allEfforts.filter(predicate);
+		if (efforts.length > 0) {
+			let header = this.createH1(title);
 			el.appendChild(header);
 
-			let sorted = todayEfforts.sort(Comparator.comparing((e: Effort) => e.due!.getTime()));
-			const dvTable = await this.createTable(sorted);
+			const dvTable = await this.createTable(efforts);
 			el.appendChild(dvTable)
 		}
 	}
 
 	private async createTable(efforts: Effort[]) {
-		const headers = ["Effort", "Area", "Status", "Votes"];
+		const headers = ["Effort", "Area", "Status"];
 
 		const rowsComparator = Comparator.combineCompare(
 			EffortStatusComparator.compare,
@@ -84,9 +86,7 @@ export default class DailyNoteLayout extends AbstractLayout<DailyNote> {
 			.map(e => {
 				const effortLink = this.toLink(e);
 				const aresStr = e.area?.name ?? "--"; // TODO use inherited area
-				const statusStr = e.status;
-				const votesStr = e.getVotes();
-				return [effortLink, aresStr, statusStr, votesStr];
+				return [effortLink, aresStr, e.status];
 			});
 		return await this.dvRenderer.table(headers, rows);
 	}
