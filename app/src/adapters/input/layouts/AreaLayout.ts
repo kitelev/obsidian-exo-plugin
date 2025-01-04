433844b47cc2547009c921ebec676c0f454e2b46
kitelev <kitelev.andrey@gmail.com>
2025-01-04 21:17:26 +0500

    Add effort sorting by status

--- app/src/adapters/input/layouts/AreaLayout.ts
+++ app/src/adapters/input/layouts/AreaLayout.ts
@@ -3,6 +3,7 @@ import ExoContext from "../../../../../common/ExoContext";
 import Area from "../../../../../core/src/domain/Area";
 import DvRenderer from "../../../utils/dv/DvRenderer";
 import Effort from "../../../../../core/src/domain/effort/Effort";
+import {EffortStatusComparator} from "../../../../../core/src/domain/effort/EffortStatus";
 
 export default class AreaLayout implements Layout<Area> {
     constructor(private ctx: ExoContext, private dvRender: DvRenderer) {
@@ -33,15 +34,17 @@ export default class AreaLayout implements Layout<Area> {
         return h1;
     }
 
-    private async createTable(unresolvedEfforts: Effort[]) {
+    private async createTable(efforts: Effort[]) {
         const headers = ["Effort", "Area", "Status", "Votes"];
-        const rows = unresolvedEfforts.map(e => {
-            const effortLink = this.toLink(e);
-            const aresStr = e.area?.name ?? "--"; // TODO use inherited area
-            const statusStr = e.status;
-            const votesStr = "--"; // TODO implement votes
-            return [effortLink, aresStr, statusStr, votesStr];
-        });
+        const rows = efforts
+            .sort(EffortStatusComparator.compare)
+            .map(e => {
+                const effortLink = this.toLink(e);
+                const aresStr = e.area?.name ?? "--"; // TODO use inherited area
+                const statusStr = e.status;
+                const votesStr = "--"; // TODO implement votes
+                return [effortLink, aresStr, statusStr, votesStr];
+            });
         return await this.dvRender.table(headers, rows);
     }
 
--- core/src/domain/effort/EffortStatus.ts
+++ core/src/domain/effort/EffortStatus.ts
@@ -1,8 +1,29 @@
+import Effort from "./Effort";
+
 export enum EffortStatus {
 	DRAFT = "DRAFT",
 	BACKLOG = "BACKLOG",
 	READY = "READY",
+	PLANNED = "PLANNED",
+	TUPLYU = "TUPLYU",
 	STARTED = "STARTED",
 	ENDED = "ENDED",
 	TRASHED = "TRASHED"
+}
+
+export class EffortStatusComparator {
+	private static statusOrder: Record<string, number> = {
+		"DRAFT": 1,
+		"BACKLOG": 2,
+		"READY": 3,
+		"PLANNED": 4,
+		"TUPLYU": 5,
+		"STARTED": 6,
+		"ENDED": 7,
+		"TRASHED": 8
+	};
+
+	static compare(e1: Effort, e2: Effort): number {
+		return (EffortStatusComparator.statusOrder[e2.status] || 99) - (EffortStatusComparator.statusOrder[e1.status] || 99);
+	}
 }
