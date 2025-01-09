93a21712d9dc62881087dd99106f99fbc651b17e
kitelev <kitelev.andrey@gmail.com>
2025-01-09 10:24:55 +0500

    Refactor (maybe not working)

--- app/src/adapters/output/serde/EffortSerde.ts
+++ app/src/adapters/output/serde/EffortSerde.ts
@@ -1,61 +1,9 @@
 import AbstractSerde from "./AbstractSerde";
-import Effort, {EffortBuilder} from "../../../../../core/src/domain/ems/effort/Effort";
-import DateUtils from "../../../../../common/utils/DateUtils";
+import {EnumFieldType, Field, KObjectFieldType, TimestampFieldType} from "./SerdeFields";
 import {FrontMatterCache, TFile} from "obsidian";
 import {UUID} from "node:crypto";
+import Effort, {EffortBuilder} from "../../../../../core/src/domain/ems/effort/Effort";
 import {EffortStatus} from "../../../../../core/src/domain/ems/effort/EffortStatus";
-import KObject from "../../../../../core/src/domain/KObject";
-import ExoContext from "../../../../../common/ExoContext";
-
-export class Field {
-	public readonly fmPropName: string;
-	public readonly koPropName: keyof EffortBuilder;
-	public readonly type: FieldType<any>;
-}
-
-export interface FieldType<T> {
-	de(ctx: ExoContext, str: string): Promise<T>;
-
-	ser(ctx: ExoContext, value: T): string;
-}
-
-export class TimestampFieldType implements FieldType<Date> {
-	async de(ctx: ExoContext, str: string): Promise<Date> {
-		return new Date(str);
-	}
-
-	ser(ctx: ExoContext, value: Date): string {
-		return DateUtils.formatTimestamp(value);
-	}
-}
-
-export class EnumFieldType<T> implements FieldType<T> {
-	constructor(public readonly enumValues: T[]) {
-	}
-
-	async de(ctx: ExoContext, str: string): Promise<T> {
-		let res = this.enumValues.filter(value => `${value}` == str).first();
-		if (!res) {
-			throw new Error("Invalid enum value: " + str);
-		}
-		return res;
-	}
-
-	ser(ctx: ExoContext, value: T): string {
-		return `${value}`;
-	}
-}
-
-export class KObjectFieldType implements FieldType<KObject> {
-	async de(ctx: ExoContext, str: string): Promise<KObject> {
-		const file = ctx.appUtils.getFileFromStrLink(str);
-		return await ctx.kObjectCreator.createFromFileTyped(file);
-	}
-
-	ser(ctx: ExoContext, ko: KObject): string {
-		return `'[[${ko.title}]]'`;
-	}
-}
 
 export default class EffortSerde extends AbstractSerde<Effort> {
 
---
+++ app/src/adapters/output/serde/SerdeFields.ts
@@ -0,0 +1,79 @@
+import {EffortBuilder} from "../../../../../core/src/domain/ems/effort/Effort";
+import ExoContext from "../../../../../common/ExoContext";
+import DateUtils from "../../../../../common/utils/DateUtils";
+import KObject from "../../../../../core/src/domain/KObject";
+
+export class Field {
+	public readonly fmPropName: string;
+	public readonly koPropName: keyof EffortBuilder;
+	public readonly type: FieldType<any>;
+}
+
+export interface FieldType<T> {
+	de(ctx: ExoContext, str: string): Promise<T>;
+
+	ser(ctx: ExoContext, value: T): string;
+}
+
+export class TimestampFieldType implements FieldType<Date> {
+	async de(ctx: ExoContext, str: string): Promise<Date> {
+		return new Date(str);
+	}
+
+	ser(ctx: ExoContext, value: Date): string {
+		return DateUtils.formatTimestamp(value);
+	}
+}
+
+export class NumberFieldType implements FieldType<number> {
+	async de(ctx: ExoContext, str: string): Promise<number> {
+		return parseInt(str);
+	}
+
+	ser(ctx: ExoContext, value: number): string {
+		return `${value}`;
+	}
+}
+
+export class EnumFieldType<T> implements FieldType<T> {
+	constructor(public readonly enumValues: T[]) {
+	}
+
+	async de(ctx: ExoContext, str: string): Promise<T> {
+		let res = this.enumValues.filter(value => `${value}` == str).first();
+		if (!res) {
+			throw new Error("Invalid enum value: " + str);
+		}
+		return res;
+	}
+
+	ser(ctx: ExoContext, value: T): string {
+		return `${value}`;
+	}
+}
+
+export class KObjectFieldType implements FieldType<KObject> {
+	async de(ctx: ExoContext, str: string): Promise<KObject> {
+		const file = ctx.appUtils.getFileFromStrLink(str);
+		return await ctx.kObjectCreator.createFromFileTyped(file);
+	}
+
+	ser(ctx: ExoContext, ko: KObject): string {
+		return `'[[${ko.title}]]'`;
+	}
+}
+
+export class ArrayFieldType<T> implements FieldType<T[]> {
+	constructor(private readonly innerType: FieldType<T>) {
+	}
+
+	async de(ctx: ExoContext, str: string): Promise<T[]> {
+		let arr = str.split("\n");
+		let strLinks = arr.map(line => line.replace(" - ", ""));
+		return await Promise.all(strLinks.map(async str => await this.innerType.de(ctx, str)));
+	}
+
+	ser(ctx: ExoContext, koArr: T[]): string {
+		return koArr.map(value => this.innerType.ser(ctx, value)).join(",");
+	}
+}
